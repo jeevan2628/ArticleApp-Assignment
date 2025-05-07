a24231aadf638a693924a8b3bdf0afc6334a1cd3
jeevan2628 <jeevan.rao@robosoftin.com>
2025-05-07 21:52:23 +0530

    [JR] initial files

---
+++ ArticleApp-Assignment/ArticleApp_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleApp_AssignmentApp.swift
+//  ArticleApp-Assignment
+//
+//  Created by Jeevan Rao on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ArticleApp_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
